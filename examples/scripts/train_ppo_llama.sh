5d8e6bb4f5c2cc472702c899ca742b878c003acf
Song Huatong <3151273556@qq.com>
2024-06-26 23:56:25 +0800

    Update train_ppo_llama.sh

--- examples/scripts/train_ppo_llama.sh
+++ examples/scripts/train_ppo_llama.sh
@@ -4,7 +4,7 @@ read -r -d '' training_commands <<EOF
 /home/wxt/huatong/OpenRLHF/examples/scripts/train_ppo.py \
     --pretrain /root/.cache/huggingface/hub/llama2_sft_mirror/ \
     --reward_pretrain OpenLLMAI/Llama-2-7b-rm-anthropic_hh-lmsys-oasst-webgpt \
-    --save_path ./ckpt/7b_llama \
+    --save_path /root/.cache/huggingface/hub/7b_llama_ppo_openrlhf \
     --save_steps -1 \
     --logging_steps 1 \
     --eval_steps -1 \
