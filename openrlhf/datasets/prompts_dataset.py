16acc0ed415aa303d437f4ebeef149eb927ef28f
Song Huatong <3151273556@qq.com>
2024-06-30 23:08:42 +0800

    Update prompts_dataset.py

--- openrlhf/datasets/prompts_dataset.py
+++ openrlhf/datasets/prompts_dataset.py
@@ -2,7 +2,43 @@ from torch.utils.data import Dataset
 from tqdm import tqdm
 from .utils import exist_and_not_none, process_multi_turn_dialogue
 
+def preprocess_data(data, input_template=None, input_key=None, apply_chat_template=None) -> str:
+    # custom dataset
+    if input_key:
+        if apply_chat_template:
+            prompt_now = data[input_key]
+            messages = [{"role": "user", "content": data[input_key]},]
+                
+            prompt = apply_chat_template(messages, tokenize=False, add_generation_prompt=True,return_tensors="pt")
+            input_template = None
+        else:
+            prompt = data[input_key]
+    else:
+        # Open-Orca/OpenOrca
+        if exist_and_not_none(data, "system_prompt") and exist_and_not_none(data, "response"):
+            prompt = data["system_prompt"] + " " + data["question"]
+        # Dahoas/full-hh-rlhf
+        elif exist_and_not_none(data, "prompt"):
+            prompt = data["prompt"]
+            # tasksource/oasst1_pairwise_rlhf_reward
+            if prompt.startswith("prompter:"):
+                prompt = (
+                    prompt.replace("prompter:", "\nHuman: ").replace("assistant:", "\nAssistant: ") + "\nAssistant: "
+                )
+            input_template = None  # do not modified with input template again
+        # RLHFlow/prompt-collection-v0.1
+        elif exist_and_not_none(data, "context_messages") and isinstance(data["context_messages"], list):
+            prompt = data["context_messages"]
+            prompt = process_multi_turn_dialogue(prompt, input_template=input_template)
+            input_template = None  # do not modified with input template again
+        else:
+            raise ValueError("Unknown prompts dataset")
 
+    # input template
+    if input_template:
+        prompt = input_template.format(prompt)
+    return prompt
+    
 def preprocess_data(data, input_template=None, input_key=None, apply_chat_template=None) -> str:
 
     print("hereherehereherh")
